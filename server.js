1b0026b302ea68549384812880ce9af31acf38f1
elisalien <43992996+elisalien@users.noreply.github.com>
2025-07-03 00:32:57 +0200

    Fixing admin eraser

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 // server.js
 const express = require('express');
 const app = express();
+const path = require('path');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/public'));
+// Serve static files from 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for admin interface
+app.get('/chantilly', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
 
 io.on('connection', socket => {
   console.log('a user connected');
 
-  // Repartir un dessin
+  // Broadcast drawing events
   socket.on('draw', data => {
     socket.broadcast.emit('draw', data);
   });
 
-  // Repartir une suppression de forme
+  // Broadcast shape deletion
   socket.on('deleteShape', ({ id }) => {
     io.emit('deleteShape', { id });
   });
 
-  // Repartir commande clear canvas
+  // Broadcast clear canvas
   socket.on('clearCanvas', () => {
     io.emit('clearCanvas');
   });
